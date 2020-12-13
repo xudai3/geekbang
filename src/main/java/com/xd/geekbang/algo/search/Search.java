e824b036339cb07cc08ec7caa755a5aa4107a8a5
xudai <xudai3@qq.com>
2020-12-13 20:22:04 +0800

    add binary search method and test case

--- src/main/java/com/xd/geekbang/algo/search/Search.java
+++ src/main/java/com/xd/geekbang/algo/search/Search.java
@@ -1,19 +1,76 @@
 package com.xd.geekbang.algo.search;
 
 public class Search {
-    public int bsearch(int[] a, int value) {
-        int len = a.length;
+    public int binarySearch(int[] a, int val) {
         int low = 0;
-        int high = len-1;
+        int high = a.length - 1;
 
         while(low <= high) {
-            int mid = (low + high) / 2;
-            if (a[mid] > value) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > val) {
                 high = mid - 1;
-            } else if (a[mid] < value) {
+            } else if (a[mid] < val) {
                 low = mid + 1;
             } else {
                 return mid;
+            }
+        }
+        return -1;
+    }
+
+    public int binarySearchRecursive(int[] a, int val) {
+        return binarySearchRecursiveInternal(a, 0, a.length-1, val);
+    }
+
+    public int binarySearchRecursiveInternal(int []a, int low, int high, int val) {
+        if (low > high) return -1;
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] > val) {
+            return binarySearchRecursiveInternal(a, low, mid-1, val);
+        } else if (a[mid] < val) {
+            return binarySearchRecursiveInternal(a, mid+1, high, val);
+        } else {
+            return mid;
+        }
+    }
+
+    public int binarySearchFirstElem(int []a, int val) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > val) {
+                high = mid - 1;
+            } else if (a[mid] < val) {
+                low = mid + 1;
+            } else {
+                if ((mid == 0) || (a[mid-1] != val)) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int binarySearchLastElem(int[] a, int val) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > val) {
+                high = mid - 1;
+            } else if (a[mid] < val) {
+                low = mid + 1;
+            } else {
+                if ((mid == a.length - 1) || (a[mid+1] != val)) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
             }
         }
         return -1;
--- src/test/java/com/xd/geekbang/algo/search/SearchTest.java
+++ src/test/java/com/xd/geekbang/algo/search/SearchTest.java
@@ -6,31 +6,74 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SearchTest {
-    ArrayList<TestCases> testCases = new ArrayList<>();
-    public SearchTest() {
-        TestCases tc1 = new TestCases();
-        tc1.inputArray = new int[]{1,2,3,4,5,6,7,8,9,10};
-        tc1.target = 1;
-        tc1.expect = 0;
-        testCases.add(tc1);
-        TestCases tc2 = new TestCases();
-        tc2.inputArray = new int[]{1,2,3,4,5,6,7,8,9,10};
-        tc2.target = 11;
-        tc2.expect = -1;
-        testCases.add(tc2);
-    }
-
-    @Test
-    void TestBSearch() {
-       Search s = new Search();
-       for (TestCases tc : testCases) {
-           assertEquals(tc.expect, s.bsearch(tc.inputArray, tc.target));
-       }
-    }
-
     class TestCases {
         int[] inputArray;
         int target;
         int expect;
+    }
+    ArrayList<TestCases> nonDuplicateCases = new ArrayList<>();
+    ArrayList<TestCases> firstEqualElemCases = new ArrayList<>();
+    ArrayList<TestCases> lastEqualElemCases = new ArrayList<>();
+    public SearchTest() {
+       TestCases tc1 = new TestCases();
+        tc1.inputArray = new int[]{1,2,3,4,5,6,7,8,9,10};
+        tc1.target = 1;
+        tc1.expect = 0;
+        nonDuplicateCases.add(tc1);
+        TestCases tc2 = new TestCases();
+        tc2.inputArray = new int[]{1,2,3,4,5,6,7,8,9,10};
+        tc2.target = 11;
+        tc2.expect = -1;
+        nonDuplicateCases.add(tc2);
+        TestCases tc3 = new TestCases();
+        tc3.inputArray = new int[]{1,3,4,5,6,8,8,8,11,13};
+        tc3.target = 8;
+        tc3.expect = 5;
+        firstEqualElemCases.add(tc3);
+        TestCases tc4 = new TestCases();
+        tc4.inputArray = new int[]{1,1,1,3,4,5,6,8,8,11,13};
+        tc4.target = 1;
+        tc4.expect = 0;
+        firstEqualElemCases.add(tc4);
+        TestCases tc5 = new TestCases();
+        tc5.inputArray = new int[]{1,3,4,5,6,8,8,8,11,13};
+        tc5.target = 8;
+        tc5.expect = 7;
+        TestCases tc6 = new TestCases();
+        tc6.inputArray = new int[]{1,3,4,5,6,8,8,11,13,13,13};
+        tc6.target = 13;
+        tc6.expect = 10;
+    }
+
+    @Test
+    void TestBinarySearch() {
+       Search s = new Search();
+       for (TestCases tc : nonDuplicateCases) {
+           assertEquals(tc.expect, s.binarySearch(tc.inputArray, tc.target));
+       }
+    }
+
+    @Test
+    void TestBinarySearchRecursive() {
+        Search s = new Search();
+        for (TestCases tc : nonDuplicateCases) {
+            assertEquals(tc.expect, s.binarySearchRecursive(tc.inputArray, tc.target));
+        }
+    }
+
+    @Test
+    void TestBinarySearchFirstElem() {
+        Search s = new Search();
+        for (TestCases tc : firstEqualElemCases) {
+            assertEquals(tc.expect, s.binarySearchFirstElem(tc.inputArray, tc.target));
+        }
+    }
+
+    @Test
+    void TestBinarySearchLastElem() {
+        Search s = new Search();
+        for (TestCases tc : lastEqualElemCases) {
+            assertEquals(tc.expect, s.binarySearchLastElem(tc.inputArray, tc.target));
+        }
     }
 }
